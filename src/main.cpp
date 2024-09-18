b47c114bc9a77417036d091ff5a7cfe144dcc846
MichalHerda <michalherda83@gmail.com>
2024-09-18 22:19:19 +0200

    initialization - tbc

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/Checkers/Main.qml"));
+    const QUrl url(QStringLiteral("qrc:/Checkers/gui/Main.qml"));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
